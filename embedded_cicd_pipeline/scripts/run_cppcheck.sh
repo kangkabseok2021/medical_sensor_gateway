3e90a20eafe1c11ea0f48c978d46c0322f117472
Kab Seok Kang <83808276+kangkabseok2021@users.noreply.github.com>
2026-05-28 09:51:35 +0200

    fix: suppress cppcheck informational messages that cause failure

--- embedded_cicd_pipeline/scripts/run_cppcheck.sh
+++ embedded_cicd_pipeline/scripts/run_cppcheck.sh
@@ -2,5 +2,5 @@
 set -e
 mkdir -p build-cppcheck
 cmake -S . -B build-cppcheck -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
-cppcheck --enable=all --error-exitcode=1 --suppress=missingInclude --suppress=unmatchedSuppression --inline-suppr --std=c++17 -i build-cppcheck/_deps --project=build-cppcheck/compile_commands.json --output-file=cppcheck_report.xml 2>&1 | tee cppcheck_output.txt
+cppcheck --enable=all --error-exitcode=1 --suppress=missingInclude --suppress=missingIncludeSystem --suppress=checkersReport --suppress=unmatchedSuppression --inline-suppr --std=c++17 -i build-cppcheck/_deps --project=build-cppcheck/compile_commands.json --output-file=cppcheck_report.xml 2>&1 | tee cppcheck_output.txt
 exit ${PIPESTATUS[0]}
