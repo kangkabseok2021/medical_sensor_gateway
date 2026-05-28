5db37e04467bb141b9c22ed599af869833388bca
Kab Seok Kang <83808276+kangkabseok2021@users.noreply.github.com>
2026-05-28 09:23:02 +0200

    fix: exclude googletest dependencies from cppcheck

--- embedded_cicd_pipeline/scripts/run_cppcheck.sh
+++ embedded_cicd_pipeline/scripts/run_cppcheck.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 mkdir -p build-cppcheck
-cmake -B build-cppcheck -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
-cppcheck --enable=all --error-exitcode=1 --suppress=missingInclude --suppress=unmatchedSuppression --inline-suppr --std=c++17 --project=build-cppcheck/compile_commands.json --output-file=cppcheck_report.xml 2>&1 | tee cppcheck_output.txt
+cmake -S . -B build-cppcheck -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+cppcheck --enable=all --error-exitcode=1 --suppress=missingInclude --suppress=unmatchedSuppression --inline-suppr --std=c++17 -i build-cppcheck/_deps --project=build-cppcheck/compile_commands.json --output-file=cppcheck_report.xml 2>&1 | tee cppcheck_output.txt
 exit ${PIPESTATUS[0]}
